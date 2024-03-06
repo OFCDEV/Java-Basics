eff31d21e14951fbfbef004fa21c5a32a6932fef
Debasish Bhola <devop69@gmail.com>
2024-03-06 19:41:48 +0530

    reverseArray

---
+++ CodingQues/reverseArray.java
@@ -0,0 +1,27 @@
+package CodingQues;
+
+import java.util.Arrays;
+
+public class reverseArray {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(arr));
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverse(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+        while (start<end) {
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+    static void swap(int[] arr,int index1,int index2){
+        int temp = index1;
+        index1 = index2;
+        index2 = temp;
+    }
+    
+}
