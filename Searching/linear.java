858a16d3aed59718970132d366991c283b2aef48
Debasish Bhola <devop69@gmail.com>
2024-03-06 21:51:21 +0530

    linearSearch

--- Searching/linear.java
+++ Searching/linear.java
@@ -6,15 +6,27 @@ public class linear {
             return -1;
         }
         for (int i = 0; i < arr.length; i++) {
-            int element = arr[i];
-            if (element == target) {
+            if (arr[i] == target) {
                 return i; //index num
             }
         }
         return -1;
     }
+    static boolean search(String str,char target){
+        if(str.length() == 0){
+            return false;
+        }
+        for (int index = 0; index < str.length(); index++) {
+            if (target == str.charAt(index)) {
+                return true;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int arr[] = {32,435,462,4234,13322,232,1,67};
         System.out.println(linearSearch(arr, 462));
+        String name = "devv";
+        System.out.println(search(name, 'e'));
     }
 }
